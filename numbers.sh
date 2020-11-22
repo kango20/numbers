cc689a13f93c0e5269a03e18be553b24ddf56183
Karen Ngo <kango@chapman.edu>
2020-11-21 23:06:31 -1000

    Adds numbers.sh

--- numbers.sh
+++ numbers.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+#numbers.sh
+#Karen Ngo
 
+echo -n "Enter a number: "
+
+read NUMBERS
+while echo "$NUMBERS" | egrep -v "^[0-9]+" >/dev/null 2>&1
+do
+	echo "You must enter a number"
+	echo "Enter a number: "
+	read NUMBERS
+done
+
+N=1
+while [ $N -le $NUMBERS ]
+do 
+	if [ $((N%2)) -eq 0 ]
+	then
+		echo $N "EVEN"
+	fi
+	if [ $((N%2)) -ne 0 ]
+	then
+		echo $N "ODD"
+	fi
+	N=$[$N+1]
+done
+echo "Thank you"
